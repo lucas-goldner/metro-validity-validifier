28dab84a161054f7775f84927225b50129465988
lucas-goldner <lucas.goldner@googlemail.com>
2022-01-04 19:53:15 +0100

    feat: Add new font and global styles

--- components/About.js
+++ components/About.js
@@ -6,6 +6,16 @@ const About = () => {
   return (
     <section id={styles.aboutcontainer}>
       <AboutIcon />
+      <h1 id={styles.title} className={"titleFont"}>
+        Verify your
+        <br />
+        amatwine NFT
+      </h1>
+      <p id={styles.desc} className={"descFont"}>
+        Log in via MetaMask
+        <br /> to view the metadata
+        <br /> of your bespoke NFT.
+      </p>
     </section>
   );
 };
