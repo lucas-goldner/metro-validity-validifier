c209b036fd71135a3cacff6be8646b9753d918ef
lucas-goldner <lucas.goldner@googlemail.com>
2022-01-04 18:35:42 +0100

    feat: Header finished

--- components/ConnectWallet.js
+++ components/ConnectWallet.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import Image from "next/image";
+import styles from "../styles/ConnectWallet.module.css";
 import { ethers } from "ethers";
 
 const ConnectWallet = ({ setAccount }) => {
   const [buttonText, setButtonText] = useState("Connect to MetaMask");
+  const [hover, setHover] = useState(false);
+
   const connectToMetaMask = async () => {
     try {
       const newAccounts = await ethereum.request({
@@ -10,7 +14,6 @@ const ConnectWallet = ({ setAccount }) => {
       });
       let accounts = newAccounts;
       setAccount({ accounts });
-
       setButtonText("Connected to Metamask");
     } catch (error) {
       console.error(error);
@@ -20,11 +23,45 @@ const ConnectWallet = ({ setAccount }) => {
 
   return (
     <button
+      id={
+        buttonText === "Connected to Metamask"
+          ? styles.metamasksuccess
+          : styles.metamask
+      }
+      onMouseOver={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
       onClick={() => {
         connectToMetaMask();
       }}
     >
-      {buttonText}
+      {buttonText === "Connected to Metamask" ? (
+        <Image
+          src="/metamask_fox_color.png"
+          alt="Metamask logo connected"
+          width={30}
+          height={30}
+          className={styles.metamaskicon}
+        />
+      ) : (
+        <Image
+          src="/metamask_fox_nocolor.png"
+          alt="Metamask logo disconnected"
+          width={30}
+          height={30}
+          className={styles.metamaskicon}
+        />
+      )}
+      <p
+        className={
+          buttonText === "Connected to Metamask"
+            ? styles.metamasktext
+            : hover
+            ? styles.metamasktextblack
+            : styles.metamasktext
+        }
+      >
+        {buttonText}
+      </p>
     </button>
   );
 };
