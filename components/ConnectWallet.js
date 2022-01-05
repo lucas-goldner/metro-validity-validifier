407226926acadde9bb5b2b2822cb044fc5662a0d
lucas-goldner <lucas.goldner@googlemail.com>
2022-01-05 02:00:18 +0100

    feat: Add new metamask connection method with hook

--- components/ConnectWallet.js
+++ components/ConnectWallet.js
@@ -1,11 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "../styles/ConnectWallet.module.css";
 import { ethers } from "ethers";
+import { useWeb3Modal } from "../hooks/web3";
 
 const ConnectWallet = ({ setAccount }) => {
   const [buttonText, setButtonText] = useState("Connect to MetaMask");
   const [hover, setHover] = useState(false);
+  const { connectWallet, disconnectWallet, provider, error } = useWeb3Modal();
+
+  useEffect(() => {
+    const getAddress = async () => {
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+      setAccount(address);
+
+      const networkId = await provider.getNetwork();
+      console.log(networkId);
+      setButtonText("Connected to Metamask");
+      //setNetworkId(networkId);
+      // if(chainId !== 8001)
+
+      // for erc721 mainnet and testnet
+      // setContract_721(
+      //   new ethers.Contract("0xD05a795d339886bB8Dd46cfe2ac009d7f1E48A64", abi)
+      // );
+
+      // for erc1155 mainnet
+      // if (networkId == "137")
+      //   setContract_1155(
+      //     new ethers.Contract(
+      //       "0xd52a86110c9a7597a057Ae2bB4F577B6CD42a639",
+      //       abi_1155
+      //     )
+      //   );
+      // // for erc1155 testnet
+      // else
+      //   setContract_1155(
+      //     new ethers.Contract(
+      //       "0x692d14f95012778aBb720Be8510f8eAeEaf74F44",
+      //       abi_1155
+      //     )
+      //   );
+
+      // console.log(contract_721, contract_1155);
+    };
+    if (provider) getAddress();
+    //else setSignerAddress("");
+  }, [provider]);
 
   const connectToMetaMask = async () => {
     try {
@@ -31,7 +73,7 @@ const ConnectWallet = ({ setAccount }) => {
       onMouseOver={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
       onClick={() => {
-        connectToMetaMask();
+        connectWallet();
       }}
     >
       {buttonText === "Connected to Metamask" ? (
---
+++ hooks/web3.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider,
+    options: {
+      infuraId: "196440d5d02d41dfa2a8ee5bfd2e96bd",
+    },
+  },
+};
+
+export function useWeb3Modal() {
+  const [provider, setProvider] = useState(undefined);
+  const [error, setError] = useState(null);
+  var web3Modal = { cachedProvider: null };
+
+  useEffect(() => {
+    // Client-side-only code
+    web3Modal = new Web3Modal({
+      network: "kovan",
+      cacheProvider: true,
+      providerOptions,
+    });
+  });
+
+  // Automatically connect if the provider is cashed but has not yet
+  // been set (e.g. page refresh)
+  if (web3Modal.cachedProvider && !provider) {
+    connectWallet();
+  }
+
+  async function connectWallet() {
+    try {
+      const externalProvider = await web3Modal.connect();
+      const ethersProvider = new ethers.providers.Web3Provider(
+        externalProvider
+      );
+      console.log(ethersProvider);
+      setProvider(ethersProvider);
+    } catch (e) {
+      setError("NO_WALLET_CONNECTED");
+      console.log("NO_WALLET_CONNECTED", e);
+    }
+  }
+
+  function disconnectWallet() {
+    web3Modal.clearCachedProvider();
+    setProvider(undefined);
+  }
+
+  return { connectWallet, disconnectWallet, provider, error };
+}
